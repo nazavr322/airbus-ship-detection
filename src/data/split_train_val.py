ee11a27ce26483e6e2fcebddf93a73ab617e276a
Nazar Polishchuk <nazarr.polishchuk@gmail.com>
2022-08-14 01:55:10 +0300

    Rewrote train/val split logic

--- src/data/split_train_val.py
+++ src/data/split_train_val.py
@@ -17,33 +17,35 @@ def create_parser() -> ArgumentParser:
     parser = ArgumentParser()
     parser.add_argument('data_path', help='path to the .csv file')
     parser.add_argument(
-        'out_path', help='path to the folder to store train and test files'
+        'out_path', help='path to the folder to store train and validation files'
     )
-    test_size_help_msg = """should be between 0.0 and 1.0 and represent the
-    proportion of the dataset to include in the test split."""
-    parser.add_argument('test_size', nargs='?', type=float, default=.15, 
-                        help=test_size_help_msg)
+    parser.add_argument('--val_size', nargs='?', type=int, default=2000, 
+                        help='represents the absolute number of validation samples')
     return parser
 
 
-def split_train_test(
-    data_path: str, test_size: int | float
+def split_train_val(
+    data_path: str, val_size: int | float, random_state: int = 1
 ) -> tuple[pd.DataFrame, ...]:
-    """Splits data into train and test""" 
+    """Splits data into train and validation""" 
     raw_df = pd.read_csv(os.path.join(ROOT_DIR, data_path))
-    train_df, test_df = train_test_split(
-        raw_df, test_size=test_size, random_state=RANDOM_STATE
+    train_df, val_df = train_test_split(
+        raw_df,
+        test_size=val_size,
+        random_state=random_state,
+        stratify=raw_df.ShipCount
     ) 
     train_df.reset_index(inplace=True, drop=True)
-    test_df.reset_index(inplace=True, drop=True)
-    return train_df, test_df
+    val_df.reset_index(inplace=True, drop=True)
+    return train_df, val_df
 
 
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()  # parse cmd arguments
 
-    train_df, test_df = split_train_test(args.data_path, args.test_size)
+    # perform train/val split
+    train_df, val_df = split_train_val(args.data_path, args.val_size, RANDOM_STATE)
 
     # construct full path to a target folder
     target_folder = os.path.join(ROOT_DIR, args.out_path)
@@ -55,4 +57,4 @@ if __name__ == '__main__':
     train_df.to_csv(
         os.path.join(target_folder, 'train_data.csv'), index=False
     )
-    test_df.to_csv(os.path.join(target_folder, 'test_data.csv'), index=False)
+    val_df.to_csv(os.path.join(target_folder, 'val_data.csv'), index=False)
