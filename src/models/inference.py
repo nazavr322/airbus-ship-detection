de397630b0458d6c4558e8e70bb1cbd4349fc157
Nazar Polishchuk <nazarr.polishchuk@gmail.com>
2022-08-30 20:04:39 +0300

    Reformatted output

--- src/models/inference.py
+++ src/models/inference.py
@@ -1,8 +1,10 @@
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # remove tensorflow warning messages
 from argparse import ArgumentParser
 
 import numpy as np
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 
 from src import ROOT_DIR
@@ -47,19 +49,35 @@ if __name__ == '__main__':
     out_dir = os.path.join(ROOT_DIR, args.out_dir)
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
-    
+        
     # save predictions
     for i, img in enumerate(images):
-        filename = os.path.split(args.filenames[i])[-1].split('.')[0]
-        mask = masks[i].numpy()
-        mask_fname = os.path.join(out_dir, filename + '-mask.png')
-        cv.imwrite(mask_fname, mask)
-        print('Mask successfully saved at:', mask_fname)
-        img_and_mask = cv.addWeighted(img, 0.8, mask, 0.2, 0.0)
-        full_img_fname = os.path.join(out_dir, filename + '-img_and_mask.png')
-        cv.imwrite(full_img_fname, img_and_mask)
-        print('Full image successfully saved at:', full_img_fname)
+        fig, ax = plt.subplots(1, 3, figsize=(12, 8))
+        fig.suptitle('Prediction results', fontsize=18)
+
+        # plot orig image
+        ax[0].imshow(img)
+        ax[0].set_title('Original image', fontsize=14)
+        ax[0].axis('off')
+        ax[0].grid(False)
         
+        # plot predicted mask
+        mask = masks[i].numpy()
+        ax[1].imshow(mask, 'binary_r')
+        ax[1].set_title('Predicted mask', fontsize=14)
+        ax[1].axis('off')
+        ax[1].grid(False)
 
-    
+        # plot both image and mask
+        ax[2].imshow(img)
+        ax[2].imshow(mask, 'binary_r', alpha=0.4)
+        ax[2].set_title('Image and mask', fontsize=14)
+        ax[2].axis('off')
+        ax[2].grid(False)
+        fig.tight_layout()
 
+        # save results
+        filename = os.path.split(args.filenames[i])[-1].split('.')[0]
+        full_fig_path = os.path.join(out_dir, filename + '-res.png')
+        plt.savefig(full_fig_path, bbox_inches='tight')
+        print('Results successfully saved at:', full_fig_path)
